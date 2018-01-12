04d3348b8874e78c09397c6d04ddd9966c3240d2
Bram Williams <bramwilliams@brams-macbook-pro.local>
2018-01-12 12:22:30 +0900

    Initial Commit

---
+++ Japanese2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Japanese2
+//
+//  Created by Bram Williams on 2018/01/12.
+//  Copyright © 2018 Bram Williams. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
